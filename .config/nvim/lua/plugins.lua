7325ac89c5d9f95f4fa7956ce2284407f52c6bfa
CaptaiNiveau <c.straten@proton.me>
2023-08-14 21:55:02 +0200

    Added initial neovim config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,28 @@
+vim.cmd [[packadd packer.nvim]]
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- You add plugins here  
+  use 'gaborvecsei/usage-tracker.nvim'
+
+  use 'nvim-lua/plenary.nvim'
+  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
+
+  use 'nvim-tree/nvim-web-devicons'
+  use 'lpoto/telescope-docker.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+
+  use 'MunifTanjim/nui.nvim'
+
+  use 'folke/noice.nvim'
+end)
+
