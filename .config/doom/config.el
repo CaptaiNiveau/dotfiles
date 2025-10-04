09af2e7883fe3f3c547212fe8dfed0ea9467e05f
CaptaiNiveau <c.straten@proton.me>
2025-10-04 12:58:14 +0200

    doom: discover projects in deckenburg

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -3,6 +3,16 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Projectile known directories
+(projectile-discover-projects-in-directory "~/deckenburg/git/" 1)
+(projectile-discover-projects-in-directory "~/deckenburg/synch/" 1)
+(projectile-discover-projects-in-directory "~/deckenburg/synch/git-merge-attempts/fullstack/" 1)
+
+(setq projectile-cleanup-known-projects t)
+
+;; catppuccin theme
+(setq doom-theme 'catppuccin)
+
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
@@ -32,7 +42,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -40,7 +50,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/deckenburg/org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
