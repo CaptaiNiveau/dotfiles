53ba2bdcef99a041e4399bdfc814292223eb681b
CaptaiNiveau <c.straten@proton.me>
2025-11-16 04:57:44 +0100

    nvim: only match kitty.conf with kitty treesitter

--- .config/nvim/lua/moonleay/lazy.lua
+++ .config/nvim/lua/moonleay/lazy.lua
@@ -2,63 +2,64 @@ local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 -- Auto-install lazy.nvim if not present
 if not vim.uv.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  print('Done.')
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
+    print('Done.')
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 
 require('lazy').setup({
-  -- list of plugins here
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  { 
-      'mistweaverco/discord.nvim',
-      event = "VeryLazy"
-  },
-  {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
-  {
-      'mikesmithgh/kitty-scrollback.nvim',
-      enabled = true,
-      lazy = true,
-      cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
-      event = { 'User KittyScrollbackLaunch' },
-      -- version = '*', -- latest stable version, may have breaking changes if major version changed
-      -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-      config = function()
-          require('kitty-scrollback').setup()
-      end,
-  },
-  {
-      'nvim-telescope/telescope.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  }, 
-  {
-      'tpope/vim-eunuch'
-  },
-  {
-      'mrcjkb/rustaceanvim',
-      version = '^5', -- Recommended
-      lazy = false, -- This plugin is already lazy
-  },
-  {
-      'nvim-treesitter/nvim-treesitter',
-      execute = "TSUpdate"
-  }
+    -- list of plugins here
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    { 
+        'mistweaverco/discord.nvim',
+        event = "VeryLazy"
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        'mikesmithgh/kitty-scrollback.nvim',
+        enabled = true,
+        lazy = true,
+        cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+        event = { 'User KittyScrollbackLaunch' },
+        -- version = '*', -- latest stable version, may have breaking changes if major version changed
+        -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+        config = function()
+            vim.keymap.set({ 'n' }, '<Esc>', '<Plug>(KsbCloseOrQuitAll)', {})
+            require('kitty-scrollback').setup()
+        end,
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }, 
+    {
+        'tpope/vim-eunuch'
+    },
+    {
+        'mrcjkb/rustaceanvim',
+        version = '^5', -- Recommended
+        lazy = false, -- This plugin is already lazy
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        execute = "TSUpdate"
+    }
 })
